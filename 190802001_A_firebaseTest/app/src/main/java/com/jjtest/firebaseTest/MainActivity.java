df15830c93c009ecd83fbbb197d7baa52de063fc
sim.jaejin <sim.jaejin@jtbc.co.kr>
2019-08-02 17:24:01 +0900

    Android Firebase Test

---
+++ 190802001_A_firebaseTest/app/src/main/java/com/jjtest/firebaseTest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.jjtest.firebaseTest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.crashlytics.android.Crashlytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG,"");
+        test();
+    }
+
+
+    private void test() {
+        Button crashButton = new Button(this);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Crashlytics.getInstance().crash(); // Force a crash
+            }
+        });
+
+        addContentView(crashButton, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+}
+
